d9f21ac9e73cba3febdd2d18d41f2ae52baefed3
ibragim3007 <40404277+ibragim3007@users.noreply.github.com>
2022-04-22 14:08:19 +0300

    test

--- laba6Stack/cmake-build-debug/cmake_install.cmake
+++ laba6Stack/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/ibra/Documents/4 семестр/Алгоритмы и структуры/laba5Stack
+# Install script for directory: /Users/ibra/Documents/GitHubTests/algoritms-and-structure-labs/laba6Stack
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/ibra/Documents/4 семестр/Алгоритмы и структуры/laba5Stack/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/ibra/Documents/GitHubTests/algoritms-and-structure-labs/laba6Stack/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- laba7/cmake-build-debug/cmake_install.cmake
+++ laba7/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/ibra/Documents/4 семестр/Алгоритмы и структуры/laba6
+# Install script for directory: /Users/ibra/Documents/GitHubTests/algoritms-and-structure-labs/laba7
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/ibra/Documents/4 семестр/Алгоритмы и структуры/laba6/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/ibra/Documents/GitHubTests/algoritms-and-structure-labs/laba7/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
